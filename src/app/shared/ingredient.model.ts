b988cda973eb1ce3f1ecabe865098cc7996bcd68
trishla.bhakat6@gmail.com <trishla.bhakat6@gmail.com>
2018-04-10 00:06:30 +0530

    secod commit

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+    // public name :string;
+    // public amount : number; 
+
+    // constructor(name :string,amount : number){
+    //     this.name =name;
+    //     this.amount = amount;
+    // }
+    //shortcut of previous code
+    constructor(public name: string, public amount: number) { }
+}
